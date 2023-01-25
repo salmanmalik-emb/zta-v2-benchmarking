8faaba7413b4bbf8cc5d5497638a6c90282ca336
salman-emb <salman.malik@emumba.com>
2023-01-25 18:14:15 +0500

    updated readme

--- pa-vs-ice-turn/highchart.js
+++ pa-vs-ice-turn/highchart.js
@@ -1,35 +1,38 @@
 // Data retrieved from https://www.vikjavev.no/ver/#2022-06-13,2022-06-14
 
 Highcharts.chart('container', {
-    chart: {
-      type: 'spline',
-      scrollablePlotArea: {
-        minWidth: 600,
-        scrollPositionX: 1
-      }
-    },
+  chart: {
+    type: 'spline',
+    scrollablePlotArea: {
+      minWidth: 600,
+      scrollPositionX: 1
+    }
+  },
+  title: {
+    text: 'CPU vs Throughput',
+  },
+
+  xAxis: {
     title: {
-      text: 'CPU vs Throughput',
+      text: 'Throughput'
     },
-  
-    xAxis: {
-      title: {
-        text: 'Throughput'
-      },
-     
+   
+  },
+  yAxis: {
+    title: {
+      text: 'CPU'
     },
-    yAxis: {
-      title: {
-        text: 'CPU'
-      },
-      max: 100
-    },
-    series: [{
-      name: 'Relay Node',
-      data: [[0, 0], [0, 0], [0, 0], [31.7, 4.5], [35.73, 8.48], [49.73, 9.1], [175, 40.7], [257.3, 41.9], [288, 60.5], [350.66, 56], [586.66, 53.6], [613.33, 84], [636, 83]]
-  
-    }, {
-      name: 'Connector',
-      data: [[30.8, 10], [33.73, 26.5], [34.13, 21.3], [46.9, 29], [47.1, 21], [173.33, 58.7], [240, 81.4], [244, 74.6], [272, 78], [337.33, 92.1], [549.33, 98], [594.66, 98], [609.33, 93]]
-    }],
-  });
+    tickInterval: 1
+  },
+  series: [{
+    name: 'Relay Node (Coturn)',
+    data: [[3.94, 0.18], [6.37, 0.2], [8.22, 0.25],  [9.46, 0.28], [12.4, 0.37], [35, 0.94]]
+
+  }, {
+    name: 'ZT-Connector',
+    data: [[3.81, 0.4], [6.13, 0.5], [7.98, 0.66], [9.09, 0.78], [12.3, 1], [34.13, 2.57]]
+  }, {
+    name: 'PA-5250',
+    data: [[3.8,7],[6,10],[8,12],[9,16],[12,17],[34,28]]
+  }],
+});
