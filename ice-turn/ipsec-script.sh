9acde5ad7871353cd8818da94d62e3ce0bdb7cd8
salman-emb <salman.malik@emumba.com>
2023-02-14 15:54:48 +0500

    added seperate files for ipsec and wg benchmarking scripts

--- ice-turn/ipsec-script.sh
+++ ice-turn/ipsec-script.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-
-if wg show utun151 > /dev/null 2>&1; 
-then
-    if [ ! -f /etc/wireguard/utun151.conf ]
-    then
-        touch /etc/wireguard/utun151.conf
-    fi
-    # Bring the interface down
-    wg-quick down utun151
-    rm /etc/wireguard/utun151.conf
-fi
-if [ ! -f /etc/ztna/client.json ]
+if [ ! -f /etc/ztna/ipsec-client.json ]
 then
         echo "creating json file"
         mkdir /etc/ztna
-        touch client.json
+        touch ipsec-client.json
 fi
-echo '{"wg_private_key":"","pre_shared_key":"","wg_iface_name":"utun151","wg_port":51821,"wg_mtu":1420,"wg_addr":"","tunnel_config":{"stuns":[{"uri":"stun:turnserver1.extremecloudztna.com:3479"}],"turns":[{"hostConfig":{"uri":"turn:turnserver1.extremecloudztna.com:3479"},"user":"salman1","password":"123"}],"signal_service":{"uri":"turnserver1.extremecloudztna.com:443","protocol":"https"}},"peers":[{"public_key":"8/0GtB5f2ahSBG56EFa3rGSGXwvib+IDrp4UQqNZrQg=","allowed_ips":["100.64.0.1/32"]}]}' > /etc/ztna/client.json
+echo '{"wg_private_key":"+GTl6rPRxzBQluVzL2dk9nHfiP51vUTv8bQHqmOAE0s=","pre_shared_key":"eNZoTDmGwCDXb1FwIF6ZF1c/AEUcIDVC","local_addr":"100.64.0.1/32","local_iface":"eth0","local_port":"4500","tunnel_config":{"stuns":[{"uri":"stun:turnserver1.extremecloudztna.com:3479"}],"turns":[{"hostConfig":{"uri":"turn:turnserver1.extremecloudztna.com:3479"},"user":"salman1","password":"123"}],"signal_service":{"uri":"turnserver1.extremecloudztna.com:443","protocol":"https"}},"peers":[]}' > /etc/ztna/ipsec-client.json
 
 if [ ! -f /etc/systemd/system/ice-turn-client.service ]
 then
@@ -25,7 +14,7 @@ then
         sudo cp ice-turn-client /usr/local/bin/ice-turn-client
         sudo chmod +x /usr/local/bin/ice-turn-client
         
-        wget -O ice-turn-client.service https://github.com/salmanmalik-emb/zta-v2-benchmarking/raw/main/ice-turn/build/client.service
+        wget -O ice-turn-client.service https://github.com/salmanmalik-emb/zta-v2-benchmarking/raw/main/ice-turn/build/ipsec-client.service
         sudo cp ice-turn-client.service /etc/systemd/system
         
         sudo systemctl daemon-reload
@@ -69,15 +58,5 @@ systemctl stop ice-turn-client.service
 sudo systemctl disable ice-turn-client
 sudo rm /etc/systemd/system/ice-turn-client.service
 sudo systemctl daemon-reload
-rm /etc/ztna/client.json
+rm /etc/ztna/ipsec-client.json
 rm ./packet-bouncer-client
-
-if wg show utun151 > /dev/null 2>&1; then
-    if [ ! -f /etc/wireguard/utun151.conf ]
-    then
-        touch /etc/wireguard/utun151.conf
-    fi
-    # Bring the interface down
-    wg-quick down utun151
-    rm /etc/wireguard/utun151.conf
-fi
